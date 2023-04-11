3f0118d4ea906b131ea5479af3f51b7958f235c7
xiehaijie <320974775@qq.com>
2023-04-11 22:38:01 +0800

    init dom api

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,20 @@
-// The entry file of your WebAssembly module.
+// export * from "asdom/assembly/glue";
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
+import { document } from "asdom/assembly";
+
+export function bootstrap(): void {
+  const el = document.createElement("h1");
+
+  el.setAttribute("foo", "bar");
+  const s: string = el.getAttribute("foo")!;
+
+  el.innerHTML = `<span style="font-weight: normal; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)">
+    <em>hello</em> from <strong>AssemblyScript</strong>
+  </span>`;
+  document.body!.appendChild(el);
+
+  const style = document.createElement("link");
+  style.setAttribute("rel", "stylesheet");
+  style.setAttribute("href", "./style.css");
+  document.body!.appendChild(style);
 }
